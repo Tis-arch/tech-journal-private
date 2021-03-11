5b8dc95b94fb602d21e893c8f16d25fad50ebcbd
Tis-arch <71024428+Tis-arch@users.noreply.github.com>
2021-03-11 17:27:20 -0500

    Create securessh.sh

---
+++ securessh.sh
@@ -0,0 +1,9 @@
+# Author John Tiseo
+# I did get a bit of help from someone else on this because of my general ineptitude with scripting.
+# Creates new user directory for ssh and creates keys.
+sudo useradd -m -d /home/$user -s /bin/bash $user
+sudo mkdir /home/$user/.ssh
+sudo cp /home/yourname/Tech_Journal/SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
+sudo chmod 700 /home/$user/.ssh
+sudo chmod 600 /home/$user/.ssh/authorized_keys
+sudo chown -R $user:$user /home/$user/.ssh
